c0ef1a89786e130d9efd692c0bbab49aba499b88
roshan <home@Prakashs-MacBook-Pro.local>
2014-09-22 18:55:50 +0100

    Initial Commit

---
+++ Inheritance/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Inheritance
+//
+//  Created by roshan on 22/09/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
